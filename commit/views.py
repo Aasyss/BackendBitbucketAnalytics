1aad59bc98e2686e4bf502902219b63fa87a6679
Prabin Shrestha <shprabiniw@gmail.com>
2019-09-29 23:59:52 +0545

    updated number of commits by a user in a day code

--- commit/views.py
+++ commit/views.py
@@ -71,18 +71,20 @@ class UserDateCommits(APIView):
         select_data = {"date": """strftime('%%Y-%%m-%%d', date)"""}
         # SQL eqivalent => select user,count(user) as count,strftime('%Y-%m-%d',date) as date from commit_commit where repository_id=16 group by strftime('%Y-%m-%d',date);
         date_commit_count = Commit.objects.extra(select=select_data).values('date','user').annotate(Count('user')).filter(repository=repository).order_by('date')
-        user_list = set(Commit.objects.values_list('user',flat=True).filter(repository=repository))
+        user_list = list(set(Commit.objects.values_list('user',flat=True).filter(repository=repository)))
         for commit in date_commit_count:
             if not commit['date'] in user_date_commit_counts.keys():
-                print('if')
-                user_date_commit_counts[commit['date']] = [{commit['user']: commit['user__count']}]
-                # for user in user_list:
-                #     if not user_date_commit_counts[commit['date']][0].keys == user:
-                #         user_date_commit_counts[commit['date']].append({user: 0})
-                user = user_date_commit_counts[commit['date']]
-                print(user)
+                user_date_commit_counts[commit['date']] = [{commit['user']:commit['user__count']}]
             else:
-                print('else')
-                user_date_commit_counts[commit['date']].append({commit['user'] : commit['user__count']})
+                user_date_commit_counts[commit['date']].append({commit['user']:commit['user__count']})
+
+                        # user_date_commit_counts[commit['date']].append({'user':commit['user'] , 'count':commit['user__count']})
+        user_counter = 0
+
+        for key in user_date_commit_counts:
+            dict = set().union(*(d.keys() for d in user_date_commit_counts[key]))
+            for user in user_list:
+                if not user in dict:
+                    user_date_commit_counts[key].append({user: 0})
 
         return Response(user_date_commit_counts)
